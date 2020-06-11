8aabf15d0d12bea437f87e1fd0574e6e931a7590
Arsen <ar.poryadin@yandex.ru>
2020-06-11 18:23:48 +0800

    inception

--- App.js
+++ App.js
@@ -1,36 +1,42 @@
 import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, SafeAreaView } from 'react-native';
+import Amplify, { Auth as AmplifyAuth } from 'aws-amplify'
+import config from './aws-exports'
+import { withAuthenticator } from 'aws-amplify-react-native'
 
-const instructions = Platform.select({
-  ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
-  android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
-});
+Amplify.configure(config)
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.js</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
-    </View>
-  );
+class App extends React.Component {
+  async componentDidMount() {
+    const user = await AmplifyAuth.currentAuthenticatedUser()
+    console.log('user:', user)
+  }
+
+  signOut = () => {
+    AmplifyAuth.signOut()
+      .then(() => this.props.onStateChange('signedOut'))
+      .catch(err => console.log('err: ', err))
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.title}>Hello World</Text>
+        <Text onPress={this.signOut}>Sign Out</Text>
+      </SafeAreaView>
+    )
+  }
 }
+
+export default withAuthenticator(App);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    alignItems: 'center'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+  title: {
+    fontSize: 28
+  }
+})  
--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,10 @@
+const blacklist = require('metro-config/src/defaults/blacklist');
+
 module.exports = {
+  resolver: {
+    blacklistRE: blacklist([/amplify\/#current-cloud-backend\/.*/]),
+  },
   transformer: {
     assetPlugins: ['expo-asset/tools/hashAssetFiles'],
-  },
+  }
 };
